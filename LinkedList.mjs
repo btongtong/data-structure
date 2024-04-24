ce22f9f26b18d0221b1856ac73c06eb6f2fc803a
btongtong <leeym2615@naver.com>
2024-04-24 17:28:05 +0900

    연결리스트 구현 및 테스트

---
+++ LinkedList.mjs
@@ -0,0 +1,117 @@
+// 1. 모든 데이터 출력 printAll()
+// 2. 모든 데이터 제거 clear()
+// 3. 특정 인덱스 위치 데이터 삽입 insertAt(index, data)
+// 4. 데이터 마지막 위치 데이터 삽입 insertLast(data)
+// 5. 특정 인덱스 데이터 삭제 deleteAt(index)
+// 6. 마지막 인덱스 데이터 삭제 deleteLast()
+// 7. 특정 인덱스 데이터 읽기 getNodeAt(index)
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.count = 0;
+    }
+
+    printAll() {
+        let currentNode = this.head;
+        let text = "[";
+
+        while(currentNode != null) {
+            text += currentNode.data;
+            currentNode = currentNode.next;
+
+            if(currentNode != null) {
+                text += ", ";
+            }
+        }
+
+        text += "]";
+        console.log(text);
+
+        // for(let i = 0; i < this.count; i++) {
+        //     console.log(currentNode.data);
+        //     currentNode = currentNode.next;
+        // }
+    }
+
+    clear() {
+        this.head = null;
+        this.count = 0;
+    }
+
+    insertAt(index, data) {
+        if(index > this.count || index < 0) {
+            throw  new Error("범위를 넘어갔습니다.");
+        }
+
+        let newNode = new Node(data);
+
+        if(index == 0) {
+            newNode.next = this.head;
+            this.head = newNode;
+        } else {
+            let currentNode = this.head;
+
+            for(let i = 0; i < index - 1; i++) {
+                currentNode = currentNode.next;
+            }
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+        }
+        this.count++;
+    }
+
+    insertLast(data) {
+        this.insertAt(this.count, data);
+    }
+
+    deleteAt(index) {
+        if(index >= this.count || index < 0) {
+            throw new Error("제거할 수 없습니다.");
+        }
+
+        let currentNode = this.head;
+
+        if(index == 0) {
+            let deleteNode = this.head;
+            this.head = this.head.next;
+            this.count--;
+            return deleteNode;
+        } else {
+            for(let i = 0; i < index - 1; i++) {
+                currentNode = currentNode.next;
+            }
+            let deleteNode = currentNode.next;
+            currentNode.next = deleteNode.next;
+            this.count--;
+            return deleteNode;
+        }
+    }
+
+    deleteLast() {
+        return this.deleteAt(this.count - 1);
+    }
+
+    getNodeAt(index) {
+        if(index >= this.count || index < 0) {
+            throw new Error("범위를 넘어갔습니다.");
+        }
+
+        let currentNode = this.head;
+
+        for(let i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+
+        return currentNode;
+    }
+}
+
+export { Node, LinkedList };
